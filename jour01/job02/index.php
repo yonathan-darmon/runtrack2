1dd14c8b6f36337edb919bc57cccffb24a367d1c
yonathan-darmon <yonathan.darmon@laplateforme.io>
2021-11-02 12:26:42 +0100

    Update index.php

--- jour01/job02/index.php
+++ jour01/job02/index.php
@@ -1,6 +1,6 @@
 <?php
 echo "Hello la plateforme!"; //affichage sans utiliser de valeurs
 $plat = "Hello la plateforme!"; 
-/*on utilise cette voici des variables*/
+#on utilise cette voici des variables
 echo '<br/>'. $plat;
 ?>
