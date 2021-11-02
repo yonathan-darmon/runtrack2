07aba58cd2a75522815acde3d65e48d0662e3684
yonathan-darmon <yonathan.darmon@laplateforme.io>
2021-11-02 10:37:56 +0100

    deuxieme parti du job

--- jour01/job01/index.php
+++ jour01/job01/index.php
@@ -1,6 +1,11 @@
 <?php
 $str = "LaPlateforme";
+echo $str;
 $str2 = "Vive";
 $str3 = "!";
-echo $str2.' '.$str.' '.$str3;
+echo '<br/>'.$str2.' '.$str.' '.$str3;
+$val = 6;
+echo '<br/>' .$val;
+$val2 = $val + 4;
+echo '<br/>'.$val2;
 ?>
