1aab9da29bbf40604a0807ad39ae8a92540a1f5b
yonathan-darmon <yonathan.darmon@laplateforme.io>
2021-11-03 12:02:01 +0100

    job05

---
+++ jour02/job05/index.php
@@ -0,0 +1,13 @@
+<?php
+
+for ($i=2;$i<=1000;$i++){
+    $multi=0;
+    for ($j=1;$j<=$i;$j++){
+        if($i%$j==0){
+            $multi++;
+        }
+    }
+    if($multi==2){
+        echo $i.'<br/>';
+    }
+}
