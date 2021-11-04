81275aa3ed4588a981c8a2438aa4d90fa7c097ef
yonathan-darmon <yonathan.darmon@laplateforme.io>
2021-11-04 15:17:45 +0100

    job7

---
+++ jour03/job07/index.php
@@ -0,0 +1,10 @@
+<?php
+$str="Certaines choses changent, et d'autres ne changeront jamais.";
+for($i=0;isset($str[$i]);$i++){
+}
+for($j=1; $j<=$i-2;$j++){
+echo $str[$j];
+}
+echo $str[$i-1];
+echo $str[0];
+?>
