f220f6924e6669e96f2699bff87f758ad298eb92
yonathan-darmon <yonathan.darmon@laplateforme.io>
2021-11-05 09:00:19 +0100

    Update index.php

--- jour03/job07/index.php
+++ jour03/job07/index.php
@@ -1,7 +1,6 @@
 <?php
 $str="Certaines choses changent, et d'autres ne changeront jamais.";
-for($i=0;isset($str[$i]);$i++){
-}
+for($i=0;isset($str[$i]);$i++);
 for($j=1; $j<=$i-2;$j++){
 echo $str[$j];
 }
