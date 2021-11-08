008f6b7ee5fdef3f07cea67d0358e96817fa1cb6
yonathan-darmon <yonathan.darmon@laplateforme.io>
2021-11-08 10:24:40 +0100

    job04

--- jour05/job04/index.php
+++ jour05/job04/index.php
@@ -1,3 +1,20 @@
 <?php
-
+function calcule($a, $operation, $b){
+   if($operation=="+"){
+       $result = $a+$b;
+   }
+   elseif($operation=="-"){
+        $result = $a-$b;
+   }
+   elseif($operation== "*"){
+       $result = $a*$b;
+   }
+   elseif($operation== "/"){
+       $result = $a/$b;
+   }
+   elseif ($operation=="%"){
+       $result = $a%$b; 
+   }
+   return $result;
+}
 ?>
