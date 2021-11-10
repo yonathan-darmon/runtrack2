58db27c3136e893b3c16dca72098647712c0898a
yonathan-darmon <yonathan.darmon@laplateforme.io>
2021-11-10 17:55:49 +0100

    job05 ça se on y est presque

--- jour07/job05/index.php
+++ jour07/job05/index.php
@@ -1,43 +1,18 @@
 <?php
 session_start();
-if(!isset($_SESSION['joueur'])){
-        $_SESSION['joueur']=true;
+if(isset($_GET['reset'])){
+    unset($_SESSION['plateau']);
 }
-if(!isset($_SESSION['hd'])){
-    $_SESSION['hd']="";
+if (!isset($_SESSION['plateau'])) {
+    $_SESSION ['plateau'] = [
+        [0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0]
+    ];
 }
-if(!isset($_SESSION['cg'])){
-    $_SESSION['cg']="";
+if (!isset($_SESSION['tour'])){
+    $_SESSION['tour'] = true;
 }
-if(!isset($_SESSION['cc'])){
-    $_SESSION['cc']="";
-}
-if(!isset($_SESSION['cd'])){
-    $_SESSION['cd']="";
-}
-if(!isset($_SESSION['bg'])){
-    $_SESSION['bg']="";
-}
-if(!isset($_SESSION['bc'])){
-    $_SESSION['bc']="";
-}
-if(!isset($_SESSION['bd'])){
-    $_SESSION['bd']="";
-}
-function clique($case){
-
-    if (isset($case) && $_SESSION['joueur']==true){
-        $affiche='X';
-        $_SESSION['joueur']=false;
-    
-    }
-    elseif(isset($case)&& $_SESSION['joueur']==false){
-        $affiche='O';
-        $_SESSION['joueur']=true;
-    }
-    return $affiche;
-}
-
 
 ?>
 <!DOCTYPE html>
@@ -49,83 +24,234 @@ function clique($case){
     <title>Document</title>
 </head>
 <body>
-    <table border="2px" >
+<table border="8px">
+    <form action="#" method="get">
         <tr>
             <td><?php
-            
-            if(!isset($_SESSION['hg'])){
-                $_SESSION['hg']="";
-                             echo' <form action="#" method="get">
-                            <input type="submit" value="-" name="hg">
-                        </form>';
-                       
-            }
-            if(isset($_GET['hg'])&& $_SESSION['hg']!= "X" && $_SESSION['hg']!= "O"){
-  
-               $_SESSION['hg']= clique($_GET['hg']);
-               echo $_SESSION['hg'];
+                if ($_SESSION['plateau'][0][0] === 1) {
+                    echo 'X';
+                } elseif ($_SESSION['plateau'][0][0] === 2) {
+                    echo 'O';
+                }
 
-               }
+                if (isset($_GET['hg']) && $_SESSION['tour']===true && $_SESSION['plateau'][0][0]==0) {
+                    $_SESSION['plateau'][0][0] = 1;
+                    echo 'X';
+                    $_SESSION['tour'] = false;
+                    var_dump($_SESSION['tour']);
+                }
 
-                ?>
-            </td>
-            <td>
-            <?php
-            if(!isset($_SESSION['hc'])){
-                $_SESSION['hc']="";
-                echo' <form action="#" method="get">
-                <input type="submit" value="-" name="hc">
-            </form>';
-            }
-               if(isset($_GET['hc'])&& $_SESSION['hc']!= "X" && $_SESSION['hc']!= "O"){
-  
-               $_SESSION['hc']= clique($_GET['hc']);
-               echo $_SESSION['hc'];
+                elseif (isset($_GET['hg'])&& $_SESSION['tour']===false && $_SESSION['plateau'][0][0]==0) {
+                    $_SESSION['plateau'][0][0] = 2;
+                    echo 'X';
+                    $_SESSION['tour'] = true;
 
-               }
+                } elseif (!isset($_GET['hg']) && $_SESSION['plateau'][0][0] === 0) {
+                    echo '<input type="submit" value="-" name="hg">';
+                }
 
-                ?>
-            </td>
-            <td>
-                <form action="#" method="get">
-                    <input type="submit" value="-" name="hd">
-                </form>
-            </td>
+
+                ?></td>
+            <td><?php
+                
+                if ($_SESSION['plateau'][0][1] === 1) {
+                    echo 'X';
+                } elseif ($_SESSION['plateau'][0][1] === 2) {
+                    echo 'O';
+                }
+
+                if (isset($_GET['hc'])&& $_SESSION['tour']===true && $_SESSION['plateau'][0][1]==0) {
+                    $_SESSION['plateau'][0][1] = 1;
+                    echo 'X';
+                    $_SESSION['tour'] = false;
+
+                } 
+                elseif (isset($_GET['hc'])&& $_SESSION['tour']===false && $_SESSION['plateau'][0][1]==0) {
+                    $_SESSION['plateau'][0][1] = 2;
+                    echo 'X';
+                    $_SESSION['tour'] = true;
+                }
+                elseif (!isset($_GET['hc']) && $_SESSION['plateau'][0][1] === 0) {
+                    echo '<input type="submit" value="-" name="hc">';
+                }
+
+
+                ?></td>
+            <td><?php
+                if ($_SESSION['plateau'][0][2] === 1) {
+                    echo 'X';
+                } elseif ($_SESSION['plateau'][0][2] === 2) {
+                    echo 'O';
+                }
+
+                if (isset($_GET['hd'])&& $_SESSION['tour']===true && $_SESSION['plateau'][0][2]==0) {
+                    $_SESSION['plateau'][0][2] = 1;
+                    echo 'X';
+                    $_SESSION['tour'] = false;
+
+                } 
+                elseif (isset($_GET['hd'])&& $_SESSION['tour']===false && $_SESSION['plateau'][0][2]==0) {
+                    $_SESSION['plateau'][0][2] = 2;
+                    echo 'X';
+                    $_SESSION['tour'] = true;
+                }
+                elseif (!isset($_GET['hd']) && $_SESSION['plateau'][0][2] === 0) {
+                    echo '<input type="submit" value="-" name="hd">';
+                }
+
+
+                ?></td>
         </tr>
         <tr>
-        <td>
-                <form action="#" method="get">
-                    <input type="submit" value="-" name="cg">
-                </form>
-            </td>
-            <td>
-                <form action="#" method="get">
-                    <input type="submit" value="-" name="cc">
-                </form>
-            </td>
-            <td>
-                <form action="#" method="get">
-                    <input type="submit" value="-" name="cd">
-                </form>
-            </td>
+            <td><?php
+                if ($_SESSION['plateau'][1][0] === 1) {
+                    echo 'X';
+                } elseif ($_SESSION['plateau'][1][0] === 2) {
+                    echo 'O';
+                }
+
+                if (isset($_GET['cg'])&& $_SESSION['tour']===true && $_SESSION['plateau'][1][0]==0) {
+                    $_SESSION['plateau'][1][0] = 1;
+                    echo 'X';
+                    $_SESSION['tour'] = false;
+
+                }
+                elseif (isset($_GET['cg'])&& $_SESSION['tour']===false && $_SESSION['plateau'][1][0]==0) {
+                    $_SESSION['plateau'][1][0] = 2;
+                    echo 'X';
+                    $_SESSION['tour'] = true;
+                }
+                elseif (!isset($_GET['cg']) && $_SESSION['plateau'][1][0] === 0) {
+                    echo '<input type="submit" value="-" name="cg">';
+                }
+
+
+                ?></td>
+            <td><?php
+                if ($_SESSION['plateau'][1][1] === 1) {
+                    echo 'X';
+                } elseif ($_SESSION['plateau'][1][1] === 2) {
+                    echo 'O';
+                }
+
+                if (isset($_GET['cc'])&& $_SESSION['tour']===true && $_SESSION['plateau'][1][1]==0) {
+                    $_SESSION['plateau'][1][1] = 1;
+                    echo 'X';
+                    $_SESSION['tour'] = false;
+
+                } 
+                elseif (isset($_GET['cc'])&& $_SESSION['tour']===false && $_SESSION['plateau'][1][1]==0) {
+                    $_SESSION['plateau'][1][1] = 2;
+                    echo 'X';
+                    $_SESSION['tour'] = true;
+                }
+                elseif (!isset($_GET['cc']) && $_SESSION['plateau'][1][1] === 0) {
+                    echo '<input type="submit" value="-" name="cc">';
+                }
+
+
+                ?></td>
+            <td><?php
+                if ($_SESSION['plateau'][1][2] === 1) {
+                    echo 'X';
+                } elseif ($_SESSION['plateau'][1][2] === 2) {
+                    echo 'O';
+                }
+
+                if (isset($_GET['cd'])&& $_SESSION['tour']===true && $_SESSION['plateau'][1][2]==0) {
+                    $_SESSION['plateau'][1][2] = 1;
+                    echo 'X';
+                    $_SESSION['tour'] = false;
+
+                } 
+                elseif (isset($_GET['cd'])&& $_SESSION['tour']===false && $_SESSION['plateau'][1][2]==0) {
+                    $_SESSION['plateau'][1][2] = 2;
+                    echo 'X';
+                    $_SESSION['tour'] = true;
+                }
+                elseif (!isset($_GET['cd']) && $_SESSION['plateau'][1][2] === 0) {
+                    echo '<input type="submit" value="-" name="cd">';
+                }
+
+
+                ?></td>
         </tr>
         <tr>
-        <td>
-                <form action="#" method="get">
-                    <input type="submit" value="-" name="bg">
-                </form>
-            </td>
-            <td>
-                <form action="#" method="get">
-                    <input type="submit" value="-" name="bc">
-                </form>
-            </td>
-            <td>
-                <form action="#" method="get">
-                    <input type="submit" value="-" name="bd">
-                </form>
-            </td>
+            <td><?php
+                if ($_SESSION['plateau'][2][0] === 1) {
+                    echo 'X';
+                } elseif ($_SESSION['plateau'][2][0] === 2) {
+                    echo 'O';
+                }
+
+                if (isset($_GET['bg'])&& $_SESSION['tour']===true && $_SESSION['plateau'][2][0]==0) {
+                    $_SESSION['plateau'][2][0] = 1;
+                    echo 'X';
+                    $_SESSION['tour'] = false;
+
+                } 
+                elseif (isset($_GET['bg'])&& $_SESSION['tour']===false && $_SESSION['plateau'][2][0]==0) {
+                    $_SESSION['plateau'][2][0] = 2;
+                    echo 'X';
+                    $_SESSION['tour'] = true;
+                }
+                elseif (!isset($_GET['bg']) && $_SESSION['plateau'][2][0] === 0) {
+                    echo '<input type="submit" value="-" name="bg">';
+                }
+
+
+                ?></td>
+            <td><?php
+                if ($_SESSION['plateau'][2][1] === 1) {
+                    echo 'X';
+                } elseif ($_SESSION['plateau'][2][1] === 2) {
+                    echo 'O';
+                }
+
+                if (isset($_GET['bc'])&& $_SESSION['tour']===true && $_SESSION['plateau'][2][1]==0) {
+                    $_SESSION['plateau'][2][1] = 1;
+                    echo 'X';
+                    $_SESSION['tour'] = false;
+
+                } 
+                elseif (isset($_GET['bc'])&& $_SESSION['tour']===false && $_SESSION['plateau'][2][1]==0) {
+                    $_SESSION['plateau'][2][1] = 2;
+                    echo 'X';
+                    $_SESSION['tour'] = true;
+                }
+                elseif (!isset($_GET['bc']) && $_SESSION['plateau'][2][1] === 0) {
+                    echo '<input type="submit" value="-" name="bc">';
+                }
+
+
+                ?></td>
+            <td><?php
+                if ($_SESSION['plateau'][2][2] === 1) {
+                    echo 'X';
+                } elseif ($_SESSION['plateau'][2][2] === 2) {
+                    echo 'O';
+                }
+
+                if (isset($_GET['bd'])&& $_SESSION['tour']===true && $_SESSION['plateau'][2][2]==0) {
+                    $_SESSION['plateau'][2][2] = 1;
+                    echo 'X';
+                    $_SESSION['tour'] = false;
+
+                } 
+                elseif (isset($_GET['bd'])&& $_SESSION['tour']===false && $_SESSION['plateau'][2][2]==0) {
+                    $_SESSION['plateau'][2][2] = 2;
+                    echo 'X';
+                    $_SESSION['tour'] = true;
+                }
+                elseif (!isset($_GET['bd']) && $_SESSION['plateau'][2][2] === 0) {
+                    echo '<input type="submit" value="-" name="bd">';
+                }
+
+
+                ?></td>
         </tr>
-    </table>
+        <input type="submit" value="reset" name="reset">
+    </form>
+</table>
 </body>
 </html>
