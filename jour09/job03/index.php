e1f37746d37df136ddbfaa5b90e0ae9b433e06ee
yonathan-darmon <yonathan.darmon@laplateforme.io>
2021-11-12 11:19:40 +0100

    job03

---
+++ jour09/job03/index.php
@@ -0,0 +1,41 @@
+
+<?php
+$connect= mysqli_connect("localhost","root","","jour08");
+$req= mysqli_query($connect, "SELECT prenom, nom, naissance FROM etudiants WHERE sexe = 'Femme'");
+$table= mysqli_fetch_all($req,MYSQLI_ASSOC);
+var_dump($table);
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<table border="2px">
+    <?php
+    echo "<thead><tr>";
+    foreach ($table[0] as $key =>$value) {
+        echo " <th>$key</th>";
+    }
+    echo "</tr>";
+    echo "</thead>";
+    echo "</tbody>";
+
+    foreach ($table as $key=>$value){
+        echo"<tr>";
+        foreach ($value as $value2){
+            echo "<td>$value2</td>";
+        }
+        echo"</tr>";
+    }
+    echo"</tbody>";
+
+    ?>
+</table>
+
+</body>
+</html>
